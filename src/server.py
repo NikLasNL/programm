d768fc8cdff8f45a32b138b25d46ed1c3cca389b
NikLasNL <nik-fokin-90@mail.ru>
2019-12-12 01:32:59 +0300

    Version 1.1.0

--- src/server.py
+++ src/server.py
@@ -1,3 +1,6 @@
+# Серверная часть для мессенджера
+# 12/12/2019
+# ver 1.1.0
 from twisted.internet import reactor
 from twisted.internet.protocol import ServerFactory, connectionDone
 from twisted.protocols.basic import LineOnlyReceiver
@@ -7,10 +10,6 @@ class ServerProtocol(LineOnlyReceiver):
     factory: 'Server'
     login: str = None
 
-    def connectionMade(self):
-        self.factory.clients.append(self)
-        print(self.factory.clients)
-
     def connectionLost(self, reason=connectionDone):
         self.factory.clients.remove(self)
 
@@ -18,42 +17,60 @@ class ServerProtocol(LineOnlyReceiver):
         content = line.decode()
 
         if self.login is not None:
-
             content = f"Messages from {self.login}: {content}"
 
-            if content == "User" :
-                self.sendLine("User Online" , self.factory.clients)
+            self.factory.history.append(content)
 
-            for client in self.factory.clients:
-                if client is not self:
-                    client.sendLine(content.encode())
-
+            for user in self.factory.clients:
+                user.sendLine(content.encode())
         else:
-            #            (login:admin) -> admin
-            if content.startswith("Login:"):
-                self.login = content.replace("Login:", "")
-                self.sendLine("Welcome!!!".encode())
+            #login:admin -> admin
+
+            if content.startswith("login:"):
+                login = content.replace("login:","")
+
+                for user in self.factory.clients:
+                    if user.login == login:
+                        self.sendLine("Login already exists! Try another one".encode())
+                        return
+
+                self.login = login
+                self.factory.clients.append(self)
+                self.factory.send_history(self)
+
             else:
-                self.sendLine("Invalid Login!".encode())
-
-
-            if content == "User" :
-                self.sendLine("User Online" , self.factory.clients)
-
+                self.sendLine("Invalid Login".encode())
 
 
 class Server(ServerFactory):
     protocol = ServerProtocol
     clients: list
+    history: list
 
     def startFactory(self):
-        self.clients = []
-        super().startFactory()
-        print("Server Started")
+        self.clients =[]
+        self.history=[]
+        print("Server started")
 
     def stopFactory(self):
-        print("Server Closed")
+        print("Server closed")
 
+    def send_history(self,client: ServerProtocol):
+        client.sendLine("Wellcome!".encode())
 
-reactor.listenTCP(1224, Server())
+        last_messages = self.history[-10:]
+
+        for msg in last_messages:
+            client.sendLine(msg.encode())
+
+reactor.listen(1123 , Server())
 reactor.run()
+
+
+
+
+
+
+
+
+
